e342179a1bb2a3491d7275cf80a0e638fdb99b09
NimeshGhag <ghagnimesh490@gmail.com>
2025-12-09 12:50:03 +0530

    feat : Set up authRoutes file

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const authRouter = require("../src/routes/auth.routes");
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/auth", authRouter);
 
 module.exports = app;
